8ae30cb4cdaaf210728fab2dbd41483802ab9c2e
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2022-02-16 19:54:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import curses
+from curses import wrapper
 
-print('Olá, Mundo!')
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr('Bem-Vindo ao Teste de Velocidade de Digitação!')
+    stdscr.addstr('\nAperte qualquer tecla.')
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    start_screen(stdscr)
+
+wrapper(main)

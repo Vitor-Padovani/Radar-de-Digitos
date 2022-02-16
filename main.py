edb10c8b104144a272e09ce66a6d4882ee312091
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2022-02-16 20:46:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,45 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(1, 0, f'WPM: {wpm}')
 
+    for i, char in enumerate(current):
+        if char == target[i]:
+            stdscr.addstr(0, i, char, curses.color_pair(1))
+        else:
+            stdscr.addstr(0, i, char, curses.color_pair(2))
+
+def wpm_test(stdscr):
+    target_text = 'Nao entre em panico!'
+    current_text = []
+    wpm = 0
+    
+    while True:
+        stdscr.clear()
+        
+        display_text(stdscr, target_text, current_text, wpm)
+
+        stdscr.refresh()
+
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        elif len(current_text) > len(target_text):
+            current_text.append(key)
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
+
+    wpm_test(stdscr)
 
 wrapper(main)
